400f0b99b148733f6d86ecfb79228ae4180fbf12
Roman Liskovenko <roman.l@globaldots.com>
2024-08-12 12:47:31 +0200

    Backend startup re-ordered; Backend re-initialization after a cleanup;

--- setup_platform/resources/risx-mssp/backend_entrypoint.sh
+++ setup_platform/resources/risx-mssp/backend_entrypoint.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-###
-# `FORCE_INIT` not equal to `0`
-#   - OR -
-# `${INIT_CHECK_DIR}/init_done` file do not exists
-##
-set -x
-if [ "${FORCE_INIT}321" != "0321" -o ! -f "${INIT_CHECK_DIR}/init_done" ]
-then
-  npm run prod \
-    && touch "${INIT_CHECK_DIR}/init_done"
-fi
-
 # Grab ENV vars from secrets
 cd /run/secrets
 for file in `ls`
@@ -19,5 +7,16 @@ do
   export ${file}=`cat ${file}`
 done
 cd -
+
+###
+# `FORCE_INIT` not equal to `0`
+#   - OR -
+# `${INIT_CHECK_DIR}/init_done` file do not exists
+##
+if [ "${FORCE_INIT}321" != "0321" -o ! -f "${INIT_CHECK_DIR}/init_done" ]
+then
+  npm run prod \
+    && touch "${INIT_CHECK_DIR}/init_done"
+fi
 
 npm run start
