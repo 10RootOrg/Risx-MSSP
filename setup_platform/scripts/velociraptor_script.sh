7cf4978a4c70cb39ddcefe4feb9788e2e4e04f5e
Kirill <k@kazakov.xyz>
2024-09-09 14:01:06 +0300

    PIN the velociraptor git version to the static one

--- setup_platform/scripts/velociraptor_script.sh
+++ setup_platform/scripts/velociraptor_script.sh
@@ -1,16 +1,36 @@
+#!/bin/bash
 #Reference https://github.com/Velocidex/velociraptor and https://github.com/weslambert/velocistack
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Check if home_path is provided
+if [ -z "$1" ]; then
+  printf "Usage: %s <home_path>\n" "$0"
+  exit 1
+fi
+
 home_path=$1
-git clone https://github.com/weslambert/velociraptor-docker
-cd velociraptor-docker
-cp $home_path/resources/velociraptor/docker-compose.yaml .
-cp $home_path/resources/velociraptor/entrypoint .
+SERVICE_NAME="velociraptor"
+SRC_DIR="$home_path/resources/$SERVICE_NAME"
+CURR_DIR=$(pwd)
+GIT_COMMIT=${GIT_COMMIT_VELOCIRAPTOR:-a8ff643549e08c4655152cf96fdfa89d9d19bbe8}
+
+
+printf "Preparing the %s:%s stack...\n" "$SERVICE_NAME" "$GIT_COMMIT"
+git clone https://github.com/weslambert/velociraptor-docker "$SERVICE_NAME"
+cd "$SERVICE_NAME"
+git checkout "$GIT_COMMIT"
+cp "${SRC_DIR}/docker-compose.yaml" .
+cp "${SRC_DIR}/entrypoint" .
 
 sudo docker compose build
 sudo docker compose up -d
 sleep 5
 
-sudo chmod 777 -R $home_path/scripts/velociraptor-docker/velociraptor
-sudo chmod 777 -R $home_path/scripts/velociraptor-docker/velociraptor/clients
+sudo chmod 777 -R "${CURR_DIR}/${SERVICE_NAME}/velociraptor"
+sudo chmod 777 -R "${CURR_DIR}/${SERVICE_NAME}/velociraptor/clients"
 cd velociraptor
-cp -R $home_path/resources/velociraptor/custom/ .
-sudo docker restart velociraptor
+cp -R "${SRC_DIR}/custom/" .
+sudo docker restart "${SERVICE_NAME}"
+printf "Velociraptor deployment completed successfully.\n"
