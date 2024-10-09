f9316ce2dc7f61c65513e96a23b5d6d94fbef786
Kirill <k@kazakov.xyz>
2024-10-09 16:37:16 +0300

    Timesketch

--- setup_platform/scripts/apps/timesketch.sh
+++ setup_platform/scripts/apps/timesketch.sh
@@ -4,24 +4,15 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Check if home_path is provided
-if [ -z "$1" ]; then
-  printf "Usage: %s <home_path>\n" "$0"
-  exit 1
-fi
+source "./libs/main.sh"
+define_env
+define_paths
+source "./libs/install-helper.sh"
 
-home_path=$1
-SERVICE_NAME="timesketch"
-SRC_DIR="$home_path/resources/$SERVICE_NAME"
-CURR_DIR=$(pwd)
+# Step 1: Pre-installation
+pre_install "timesketch"
 
-mkdir -p "${CURR_DIR}/${SERVICE_NAME}"
-cd "${CURR_DIR}/${SERVICE_NAME}"
+# Step 2: Run the deployment script
+sudo workdir="${workdir}" ./deploy_timesketch.sh
 
-printf "Copying the Timesketch stack configs...\n"
-cp "${SRC_DIR}/deploy_timesketch.sh" .
-cp "${SRC_DIR}/config.env" .
-cp "${SRC_DIR}/docker-compose.yml" .
-
-sudo chmod 755 deploy_timesketch.sh
-sudo ./deploy_timesketch.sh "$home_path"
+print_green_v2 "$service_name deployment started." "Successfully"
