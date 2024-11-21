1dc23ba17c14d4593521c22df951a0ccaea555d9
Kirill <k@kazakov.xyz>
2024-11-22 01:10:23 +0300

    Upd risx-mssp

--- setup_platform/scripts/apps/risx-mssp.sh
+++ setup_platform/scripts/apps/risx-mssp.sh
@@ -1,35 +1,45 @@
 #!/bin/bash
+# Reference: https://github.com/dfir-iris/iris-web/tree/master
 
-. ./_library.sh
+set -e
 
-home_path=$1
-mkdir -p risx-mssp
-cd risx-mssp
+source "./libs/main.sh"
+define_env
+define_paths
+source "./libs/install-helper.sh"
 
-cp $home_path/resources/risx-mssp/docker-compose.yaml .
-cp $home_path/resources/risx-mssp/*.Dockerfile .
-cp $home_path/resources/risx-mssp/environment.sh .
-cp $home_path/resources/risx-mssp/backend_entrypoint.sh .
-cp $home_path/resources/risx-mssp/permissions.sql .
-cp $home_path/resources/risx-mssp/nginx_default.conf.template .
+# Step 1: Pre-installation
+pre_install "risx-mssp"
 
-find $home_path/resources/risx-mssp -name 'env.*.secret' -type f | xargs -I % cp % .
-find $home_path/resources/risx-mssp -name '*.passwd' -type f | xargs -I % cp % .
+# Step 3: Generate passwords if required
+if [[ -z "$GENERATE_ALL_PASSWORDS" ]]; then
+  read -p "Would you like to generate passwords? [Y/n] (default:no)" ANSWER
+  GENERATE_ALL_PASSWORDS=${ANSWER:-n}
+fi
 
-generate_passwords_if_required .
+if [[ $GENERATE_ALL_PASSWORDS =~ ^[Yy]$ || $GENERATE_ALL_PASSWORDS =~ ^[Yy][Ee][Ss]$ ]]; then
+  source "${curr_dir}/libs/passwords.sh"
+  generate_passwords_if_required .
+fi
 
-# Grab ENV vars from secrets
-for file in `ls env.*.secret`
-do
-  # Cut `env.` prefix and `.secret` suffix from the filename
-  var=`echo ${file} | sed -e 's#^env\.##; s#\.secret##'`
-  export ${var}=`cat ${file}`
-done
-
-# Replace ENV vars with values in config file
-cat $home_path/resources/risx-mssp/mssp_config.json.envsubst | envsubst > mssp_config.json
-docker compose build
-touch mssp-back.log
-docker compose up -d
-
-cd -
+####
+#find $home_path/resources/risx-mssp -name 'env.*.secret' -type f | xargs -I % cp % .
+#find $home_path/resources/risx-mssp -name '*.passwd' -type f | xargs -I % cp % .
+#
+#generate_passwords_if_required .
+#
+## Grab ENV vars from secrets
+#for file in `ls env.*.secret`
+#do
+#  # Cut `env.` prefix and `.secret` suffix from the filename
+#  var=`echo ${file} | sed -e 's#^env\.##; s#\.secret##'`
+#  export ${var}=`cat ${file}`
+#done
+#
+## Replace ENV vars with values in config file
+#cat $home_path/resources/risx-mssp/mssp_config.json.envsubst | envsubst > mssp_config.json
+#docker compose build
+#touch mssp-back.log
+#docker compose up -d
+#
+#cd -
