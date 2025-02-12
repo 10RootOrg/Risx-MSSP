e0646e796db550c4eebffc3109df924b3357b918
D-V-W <david.v.wolfson@gmail.com>
2025-02-12 12:25:41 +0200

    fix prowler

--- setup_platform/scripts/apps/prowler.sh
+++ setup_platform/scripts/apps/prowler.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Reference: https://github.com/prowler-cloud/prowler/
 
+
+print_green_v2 "Start $service_name sh" "Start"
+
 # Exit immediately if a command exits with a non-zero status 
 set -e
 
@@ -8,13 +11,14 @@ source "./libs/main.sh"
 define_env
 define_paths
 source "./libs/install-helper.sh"
-
+print_yellow "Past part 1"
 # Step 1: Pre-installation
 pre_install "prowler"
 # replace_env "PROWLER_IMAGE_TAG"
 # replace_env "PROWLER_COMMAND"
 
 # Step 2: Start the service
+print_yellow "Start Part 2"
 printf "Starting the service...\n"
 docker compose up -d --force-recreate
 
